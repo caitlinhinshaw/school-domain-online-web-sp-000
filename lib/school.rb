ec82dc0e9295c5b8b2bef2d21cedc88ac00818bb
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-06-02 04:21:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster[grade] == false
+    if @roster.key?(grade) == false
       @roster[grade] = []
     end
     @roster[grade] << name
